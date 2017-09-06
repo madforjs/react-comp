7ebbdba8fa6296767357cc245bb99b7948b48c6d
Sai Vishnu Vippagunta <me@vishnu.guru>
2017-09-06 10:07:45 +0530

    Updated few htmls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from './Button.js';
+import Button from './atoms/Button.js';
 
 class App extends Component {
   render() {
